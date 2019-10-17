bf31c6a2e7cea51b7d00eade5ce57b2ae8946522
Divergenny <divergenny@gmail.com>
2019-10-17 21:26:49 +0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for (let row = 0; row < 9; row++) {
+    for (let column = 0; column < 9; column++) {
+        if (matrix[row][column] === 0) {
+          let options = divideAndConquer(row, column);
+            for (let option of options) {
+                matrix[row][column] = option;
+            }
+        }
+    }
+  }
+  function divideAndConquer(row,column) {
+    let data = [];
+    let square = {
+      row: Math.floor(row / 3) * 3, column: Math.floor(column / 3) * 3
+    };
+    for (let i = 0; i < 9; i++) {
+      data.push( [ matrix[row][i], matrix[i][column], matrix[square.row + i % 3][square.column + Math.floor(i / 3)] ] );
+    }
+    return data;
+  }
+  return matrix;
 }
